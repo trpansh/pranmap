edadc36c80080180ae3eefdf3e6d75ac2b6f5a4a
Saugat Acharya <mesaugat@gmail.com>
2013-10-09 14:27:53 +0545

    Database

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = 'pranmap.yipl.net';
 $db['default']['username'] = 'pranmap_admin';
 $db['default']['password'] = 'admin';
 $db['default']['database'] = 'pranmap_db';
