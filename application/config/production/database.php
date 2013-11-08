4cdb0d67a3a2fe01cc51046111eed6625738c4f5
Saugat Acharya <mesaugat@gmail.com>
2013-11-08 10:22:38 +0545

    Gitignore Update

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'pranmap_admin';
-$db['default']['password'] = 'admin';
-$db['default']['database'] = 'pranmap_db';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
